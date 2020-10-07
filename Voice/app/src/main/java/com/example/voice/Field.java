51e39208c8947dcc5e3178699dc653d38f12277c
Moonvely_i <68009161+dreamjh20@users.noreply.github.com>
2020-10-07 22:55:26 +0900

    Fourth commit

---
+++ Voice/app/src/main/java/com/example/voice/Field.java
@@ -0,0 +1,17 @@
+package com.example.voice;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Field extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.field);
+    }
+}
