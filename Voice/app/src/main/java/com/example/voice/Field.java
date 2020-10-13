82d8b960ab7f436cf0cb94429ffdcc3d7b146d86
Moonvely_i <68009161+dreamjh20@users.noreply.github.com>
2020-10-13 23:37:06 +0900

    seventeenth commit
    FrameLayout

--- Voice/app/src/main/java/com/example/voice/Field.java
+++ Voice/app/src/main/java/com/example/voice/Field.java
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Field extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
