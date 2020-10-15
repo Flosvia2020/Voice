d05af1615952e7b27c227713cc7d3f4b0ca19049
Moonvely_i <68009161+dreamjh20@users.noreply.github.com>
2020-10-15 14:41:29 +0900

    nineteenth commit

--- Voice/app/src/main/java/com/example/voice/Field.java
+++ Voice/app/src/main/java/com/example/voice/Field.java
@@ -1,6 +1,9 @@
 package com.example.voice;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
@@ -8,9 +11,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Field extends AppCompatActivity {
 
+    ImageButton pen;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.field);
+        pen = (ImageButton)findViewById(R.id.field_pen_btn);
     }
 }
