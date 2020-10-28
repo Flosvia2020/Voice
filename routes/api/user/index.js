85379d674ae694ad29d6cddde9e72b51740ed3ba
JiYoon Lee <49777530+jiyun0605@users.noreply.github.com>
2020-10-28 19:00:02 +0900

    [DELETE] ALL

--- routes/api/user/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const controller = require('./user.controller')
-
-router.get('/list', controller.list)
-router.post('/assign-admin/:id', controller.assignAdmin)
-
-module.exports = router
