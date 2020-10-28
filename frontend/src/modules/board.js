312e19c6af414d68f02f02f6d4383a955f546ede
syi0808 <syi9397@naver.com>
2020-10-28 18:50:57 +0900

    [DELETE] front, android

--- frontend/src/modules/board.js
+++
@@ -1,9 +0,0 @@
-import { handleActions } from "redux-actions";
-
-const SAVE = "SAVE";
-const REOMVE = "REMOVE";
-const READ = "READ";
-const LIST = "LIST";
-const EDIT = "EDIT";
-
-export const save = (data) => ({ type: SAVE, data });
