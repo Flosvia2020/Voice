c2345ce7b0f794dfd138e127bb5da85bbca6c0bf
문준혁 <dreamjh20@gmail.com>
2020-11-18 20:26:14 +0900

    second commit

---
+++ VOICE_ANDROID_RESTART/app/src/main/java/com/example/voice_android_restart/LoginResult.java
@@ -0,0 +1,11 @@
+package com.example.voice_android_restart;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResult {
+    private String name;
+
+//     @SerializedName("id")
+    private String id;
+}
---
+++ VOICE_ANDROID_RESTART/app/src/main/java/com/example/voice_android_restart/RetrofitInterface.java
@@ -0,0 +1,15 @@
+package com.example.voice_android_restart;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitInterface {
+    @POST("/signin")
+    Call<LoginResult> executeLogin(@Body HashMap<String, String> map);
+
+    @POST("/signup")
+    Call<Void> executeSignup (@Body HashMap<String, String> map);
+}
